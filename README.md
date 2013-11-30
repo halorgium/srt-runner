bbde91323be57502747fe861d557da499e76bd3f
Tim Carey-Smith <tim@spork.in>
2013-11-29 19:01:20 -0800

    Start of the hack

---
+++ README.md
@@ -0,0 +1,19 @@
+# SRT Runner
+
+## Setup
+
+``` sh
+bundle
+```
+
+## Usage
+
+Starting from the beginning:
+``` sh
+bundle exec ruby run.rb /path/to/movie.srt
+```
+
+Starting from an offset:
+``` sh
+bundle exec ruby run.rb /path/to/movie.srt 11:30
+```
---
+++ run.rb
@@ -0,0 +1,57 @@
+#!/usr/bin/env ruby
+
+require 'timers'
+require 'srt'
+
+def parse_timestamp(timestamp)
+  parts = timestamp.split(":")
+  offset = 0
+  parts.reverse.each_with_index do |part,index|
+    offset += (60 ** index) * Integer(part)
+  end
+  offset
+end
+
+def normalize_offset(offset)
+  offset ||= 0
+
+  case offset
+  when Integer, Float
+    offset
+  when String
+    parse_timestamp(offset)
+  else
+    raise "Unknown offset: #{offset.inspect}"
+  end
+end
+
+path = ARGV.shift || raise("Provide the path to the .srt file")
+offset = ARGV.shift
+offset = normalize_offset(offset)
+fudge = 2
+
+srt = SRT::File.parse_file(File.open(path))
+
+puts "reading from path #{path.inspect}"
+puts "starting at offset #{offset.inspect}"
+
+timers = Timers.new
+visible = Set.new
+srt.lines.each do |line|
+  timers.after(line.start_time - offset - fudge) do
+    visible.add(line)
+  end
+  timers.after(line.end_time - offset + fudge) do
+    visible.delete(line)
+  end
+end
+
+loop do
+  timers.wait
+
+  puts "\e[H\e[2J"
+
+  visible.each do |line|
+    puts line.text
+  end
+end
